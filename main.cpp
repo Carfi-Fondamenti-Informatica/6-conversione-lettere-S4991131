c9622a95d4e60454a21aef83675f8b3e6cfa8d24
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-12-01 17:09:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
+#include "lib.h"
+using namespace std;
+int main (){
+  char x;
+  cin>>x;
+  if(lettera (x) == true){
+    cout<<x<<endl;
+  } else {
+    cout<<"errore"<<endl;
   return 0;
+  }
 }
