00f7ebda7f6525843e60e8dadb42a0ad4ca6c5b2
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-12-01 17:30:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+bool lettera (char &a){
+  bool lett;
+  if (a>=65 and a<=90){
+    lett=true;
+    a=a+32;
+  }else if (a>=97 and a<=122){
+    lett=true;
+    a=a-32;
+  }else{
+    lett=false;
+  }
+  return lett;
+}  
